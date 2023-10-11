3bfedfd8dc071827809e7c02f8a44a74815ea0c5
DESKTOP-FCI4NUN\vigi0 <xu3u42209@gmail.com>
2023-10-11 23:37:28 +0800

    basic finish

--- gui.js
+++ gui.js
@@ -4,22 +4,23 @@ document.addEventListener("DOMContentLoaded", function() {
     const toggleBtn1 = d3.select("#toggleBtn1");
     const toggleBtn2 = d3.select("#toggleBtn2");
     const elementsToToggle = d3.selectAll(".construct-container, .transparent-box-acc, .construct, .accident-container, .transparent-box-con, .accident, .position-container1, .pos1");
-
+    const elementsToToggle2 = d3.selectAll(".congestion-container, .transparent-box-cng, .congestion, .position-container2, .pos2");
     elementsToToggle.classed("hidden", true);
+    elementsToToggle2.classed("hidden", true);
     toggleBtn1.on("click", function() {
         elementsToToggle.classed("hidden", true);
         toggleSidebar(sidebar1);
         hideSidebar(sidebar2);
         elementsToToggle.classed("hidden", false);
-        // elementsToToggle.classed("hidden", function(d, i) {
-        //     return !d3.select(this).classed("hidden");
-        //   });
+        elementsToToggle2.classed("hidden",true);
       });
 
       toggleBtn2.on("click", function() {
+        elementsToToggle2.classed("hidden", true);
         toggleSidebar(sidebar2);
         hideSidebar(sidebar1);
         elementsToToggle.classed("hidden", true);
+        elementsToToggle2.classed("hidden",false);
       });
       function toggleSidebar(sidebar) {
         const currentRight = parseInt(sidebar.style("left"), 10);
